d97e21daee511521aee85b451fee491939d8df58
Kirk Ding <kirk_kd@mac.home>
2026-03-27 22:12:27 -0400

    Redid target selector blocks

--- src/blocks/definitions/selectors.ts
+++ src/blocks/definitions/selectors.ts
@@ -1,26 +1,383 @@
 const targetSelectors: any[] = [
 	{
-		"type": "mc_target_selector",
-		"tooltip": "Build a target selector with optional filter arguments",
-		"helpUrl": "",
-		"message0": "target selector %1",
-		"args0": [
-			{
-				"type": "field_dropdown",
-				"name": "BASE",
-				"options": [
-					["@s", "@s"],
-					["@p", "@p"],
-					["@a", "@a"],
-					["@e", "@e"],
-					["@r", "@r"],
-					["@n", "@n"]
-				]
-			}
-		],
-		"output": "mc_selector",
-		"mutator": "mc_selector_mutator"
-	}
+    "type": "mc_target_selector",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "select %1 %2 %3",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "BASE",
+        "options": [
+          [
+            "command executer (@s)",
+            "@s"
+          ],
+          [
+            "nearest player (@p)",
+            "@p"
+          ],
+          [
+            "random player (@r)",
+            "@r"
+          ],
+          [
+            "all players (@a)",
+            "@a"
+          ],
+          [
+            "nearest entity (@n)",
+            "@n"
+          ],
+          [
+            "all entities (@e)",
+            "@e"
+          ]
+        ]
+      },
+      {
+        "type": "input_dummy",
+        "name": "BASE"
+      },
+      {
+        "type": "input_statement",
+        "name": "FILTER_STACK"
+      }
+    ],
+    "output": 'mc_target_selector',
+  },
+
+  // Basic
+  {
+    "type": "mc_target_filter_limit",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "limit %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "LIMIT",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  {
+    "type": "mc_target_filter_sort",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "sort by %1",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "SORT",
+        "options": [
+          ["increasing distance", "nearest"],
+          ["decreasing distance", "furthest"],
+          ["random", "random"],
+          ["arbitrary", "arbitrary"]
+        ]
+      },
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+
+  // Position
+  {
+    "type": "mc_target_filter_position",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "position x %1 y %2 z %3",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "X",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "Y",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "Z",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  {
+    "type": "mc_target_filter_distance",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "radius min %1 max %2",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "MIN",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "MAX",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  {
+    "type": "mc_target_filter_volume",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "volume dx %1 dy %2 dz %3",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "DX",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "DY",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "DZ",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  {
+    "type": "mc_target_filter_vert_rot",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "vertical rotation min %1 max %2",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "MIN",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "MAX",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  {
+    "type": "mc_target_filter_hori_rot",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "horizontal rotation min %1 max %2",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "MIN",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "MAX",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+
+  // Entity information
+  {
+    "type": "mc_target_filter_type",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "entity type %1",
+    "args0": [  // TODO replace with dropdown
+      {
+        "type": "field_input",
+        "name": "TYPE",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  {
+    "type": "mc_target_filter_name",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "entity name %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "TYPE",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  { // TODO custom interface
+    "type": "mc_target_filter_predicates",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "predicates %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "TYPE",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  { // TODO custom interface
+    "type": "mc_target_filter_nbt",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "NBT data %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "TYPE",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+
+  // Scoreboard
+  { // TODO custom interface
+    "type": "mc_target_filter_scores",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "scores %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "TYPE",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  { // TODO custom interface
+    "type": "mc_target_filter_tags",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "tags %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "TYPE",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  {
+    "type": "mc_target_filter_team",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "team %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "TYPE",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+
+  // Player
+  {
+    "type": "mc_target_filter_xp",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "xp min %1 max %2",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "MIN",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "MAX",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  {
+    "type": "mc_target_filter_gamemode",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "gamemode %1",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "GAMEMODE",
+        "options": [
+          ["survival", "survival"],
+          ["creative", "creative"],
+          ["adventure", "adventure"],
+          ["spectator", "spectator"]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
+  { // TODO custom interface
+    "type": "mc_target_filter_advancements",
+    "tooltip": "",
+    "helpUrl": "",
+    "message0": "advancements %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "ADVANCEMENTS",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "inputsInline": true
+  },
 ]
 
 export default targetSelectors
