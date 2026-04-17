831ecbcf3980fca96b0cf9258fdfcaca5238cc69
kirk_kd <kirk.ding123@gmail.com>
2026-04-17 15:59:17 -0400

    Add string editor

---
+++ src/editor/editors/StringEditor.tsx
@@ -0,0 +1,25 @@
+import type {StringEditorProps} from "../types.ts";
+import {useEffect, useState} from "react";
+import TextInput from "../components/TextInput.tsx";
+
+export default function StringEditor({ callback, defaultValue, className }: StringEditorProps & {className?: string}) {
+  const [value, setValue] = useState(defaultValue || '')
+
+  useEffect(() => {
+    callback({
+      error: false,
+      data: value,
+      compileValue: () => value
+    })
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <TextInput
+      defaultValue={defaultValue || ''}
+      value={value}
+      setValue={setValue}
+      className={className}
+      onChange={nextValue => callback({ error: false, data: nextValue, compileValue: () => nextValue})}
+    />
+  )
+}
