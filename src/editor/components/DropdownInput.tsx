f604d82ee3208fbb33f8cd288abf3a5b59ac7d3c
kirk_kd <kirk.ding123@gmail.com>
2026-04-25 20:19:35 -0400

    Fix dropdown input not reporting value changes.

--- src/editor/components/DropdownInput.tsx
+++ src/editor/components/DropdownInput.tsx
@@ -4,24 +4,24 @@ import {Autocomplete, type SxProps, TextField, type Theme} from "@mui/material";
 type DropdownInputProps = {
   disabled?: boolean
   options: string[]
-  value?: string
+  value?: string | null
   setValue: (newValue: string) => void
 }
 
-export default function DropdownInput({ disabled, options, value, setValue, sx }: DropdownInputProps & {sx?: SxProps<Theme>}) {
+export default function DropdownInput({ disabled, options, value = null, setValue, sx }: DropdownInputProps & {sx?: SxProps<Theme>}) {
   useEffect(() => {
-    if (options) setValue(options[0])
+    if (options?.length) setValue(options[0])
   }, [options, setValue])
 
   return <Autocomplete
     disablePortal
     options={options}
     value={value}
-    renderInput={(params) => <TextField {...params} variant={'outlined'} size={'small'}/>}
-    disabled={disabled}
-    sx={{
-      ...sx,
-      width: '100%'
+    onChange={(_, newValue) => {
+      if (newValue) setValue(newValue)
     }}
+    renderInput={(params) => <TextField {...params} variant="outlined" size="small"/>}
+    disabled={disabled}
+    sx={{ width: '100%', ...sx }}
   />
 }
