fe6c719b2b76a57b28058deb41b8255dda399513
kirk_kd <kirk.ding123@gmail.com>
2026-04-27 01:08:29 -0400

    Basic project config editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import {WorkspacePanel} from "./ui/workspace"
-import {EditorModal} from "./ui/editor"
+import {controller, EditorModal} from "./ui/editor"
 import {Box, CssBaseline, ThemeProvider} from "@mui/material";
 import theme from "./theme.ts";
+import {ProjectConfigEditor} from "./ui/editor/editors/ProjectConfigEditor.tsx";
 
 function App() {
+  controller.openEditorModal({
+    title: 'Project Configuration',
+    editor: <ProjectConfigEditor/>
+  })
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline/>
---
+++ src/ui/editor/editors/ProjectConfigEditor.tsx
@@ -0,0 +1,58 @@
+import type {EditorState, EditorStateCallback} from "../../../core/editor";
+import {type ProjectConfig, useProjectConfigStore} from "../../../stores";
+import {useEffect, useState} from "react";
+import {ObjectEditor} from "./ObjectEditor";
+import StringEditor from "./StringEditor.tsx";
+import BooleanEditor from "./BooleanEditor.tsx";
+
+function useConfigEntry<K extends keyof ProjectConfig>(key: K): [EditorState<ProjectConfig[K]>, EditorStateCallback<ProjectConfig[K]>] {
+  const [state, setState] = useState<EditorState<ProjectConfig[K]>>({
+    error: false,
+    compiler: 'scalar',
+    data: useProjectConfigStore.getState().projectConfig[key]
+  })
+
+  useEffect(() => {
+    if (state.data !== undefined) {
+      useProjectConfigStore.getState().updateConfig({ [key]: state.data } as Partial<ProjectConfig>)
+    }
+  }, [key, state.data])
+
+  useEffect(() => {
+    return useProjectConfigStore.subscribe(store => {
+      setState(prev => ({ ...prev, data: store.projectConfig[key] }))
+    })
+  }, [key])
+
+  return [state, setState]
+}
+
+export function ProjectConfigEditor() {
+  const [namespaceState, setNamespaceState] = useConfigEntry('namespace')
+  const [descriptionState, setDescriptionState] = useConfigEntry('description')
+  const [noNameManglingState, setNoNameManglingState] = useConfigEntry('noNameMangling')
+
+  return <ObjectEditor state={{ error: false, compiler: 'object' }} setState={() => {}} entries={[
+    {
+      key: 'Project namespace',
+      compiler: 'scalar',
+      component: () => (
+        <StringEditor state={namespaceState} setState={setNamespaceState}/>
+      ),
+    },
+    { // TODO multiline textfield
+      key: 'Description',
+      compiler: 'scalar',
+      component: () => (
+        <StringEditor state={descriptionState} setState={setDescriptionState}/>
+      ),
+    },
+    {
+      key: 'No name-mangling',
+      compiler: 'scalar',
+      component: () => (
+        <BooleanEditor state={noNameManglingState} setState={setNoNameManglingState}/>
+      ),
+    },
+  ]}/>
+}
