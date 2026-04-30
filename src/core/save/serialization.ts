9d19128e8927c33015e86a40fbd101c81bec7477
kirk_kd <kirk.ding123@gmail.com>
2026-04-29 23:22:13 -0400

    Add serialization for registry system

--- src/core/save/serialization.ts
+++ src/core/save/serialization.ts
@@ -1,19 +1,27 @@
-import type {BlocklyWorkspaceData, ProjectSave, SerializationOptions} from "./types.ts"
+import type {BlocklyRegistryData, BlocklyWorkspaceData, ProjectSave, SerializationOptions} from "./types.ts"
 import * as Blockly from "blockly";
 import {useProjectConfigStore} from "../../stores";
+import {procedureRegistry, variableRegistry} from "../blockly/registry";
 
 export function serialize({ workspace }: SerializationOptions): ProjectSave {
-  const blocklyWorkspace = Blockly.serialization.workspaces.save(workspace) as BlocklyWorkspaceData
+  const blocklyWorkspace: BlocklyWorkspaceData = Blockly.serialization.workspaces.save(workspace)
   const projectConfig = useProjectConfigStore.getState().projectConfig
+  const blocklyRegistry: BlocklyRegistryData = {
+    variableEntries: variableRegistry.list(),
+    procedureEntries: procedureRegistry.list()
+  }
 
   return {
     version: 1,
     blocklyWorkspace,
-    projectConfig
+    projectConfig,
+    blocklyRegistry
   }
 }
 
 export function deserialize({ save, workspace }: SerializationOptions & { save: ProjectSave }) {
   Blockly.serialization.workspaces.load(save.blocklyWorkspace, workspace)
   useProjectConfigStore.getState().replaceConfig(save.projectConfig)
+  variableRegistry.replace(save.blocklyRegistry.variableEntries)
+  procedureRegistry.replace(save.blocklyRegistry.procedureEntries)
 }
