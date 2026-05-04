4cae5cb6432aca9d54980f020c89239e76571bf9
kirk_kd <kirk.ding123@gmail.com>
2026-05-04 12:16:00 -0400

    Clean up keyboard shortcut detection using `react-hotkeys-hook` package

--- src/ui/ide/index.tsx
+++ src/ui/ide/index.tsx
@@ -7,24 +7,27 @@ import {OutputPreview} from "./OutputPreview";
 import FolderZipIcon from '@mui/icons-material/FolderZip';
 import {useIDEContext} from "./context/useIDEContext.ts";
 import {useKeyboardShortcuts} from "./useKeyboardShortcuts.ts";
+import {HotkeysProvider} from 'react-hotkeys-hook'
 
 export function IDE() {
   const { outputViewerOpen, setOutputViewerOpen } = useIDEContext()
   useKeyboardShortcuts()
 
   return (
-    <Stack sx={{ width: '100%', height: '100%', minHeight: 0 }}>
-      <MenuBar />
-      <SplitLayout>
-        <Panel dominant minWidth={'50rem'}>
-          <WorkspacePanel />
-        </Panel>
-        <Panel width={'40%'} minWidth={'20rem'} icon={<FolderZipIcon/>} title={'output preview'} open={outputViewerOpen} setOpen={setOutputViewerOpen}>
-          <OutputPreview/>
-        </Panel>
-      </SplitLayout>
-      <EditorModal />
-    </Stack>
+    <HotkeysProvider>
+      <Stack sx={{ width: '100%', height: '100%', minHeight: 0 }}>
+        <MenuBar />
+        <SplitLayout>
+          <Panel dominant minWidth={'50rem'}>
+            <WorkspacePanel />
+          </Panel>
+          <Panel width={'40%'} minWidth={'20rem'} icon={<FolderZipIcon/>} title={'output preview'} open={outputViewerOpen} setOpen={setOutputViewerOpen}>
+            <OutputPreview/>
+          </Panel>
+        </SplitLayout>
+        <EditorModal />
+      </Stack>
+    </HotkeysProvider>
   )
 }
 
--- src/ui/ide/useKeyboardShortcuts.ts
+++ src/ui/ide/useKeyboardShortcuts.ts
@@ -1,22 +1,30 @@
-import {useEffect} from "react";
-import {useIDEContext} from "./context/useIDEContext.ts";
-import {saveProject} from "../../core/save";
+import {useIDEContext} from "./context/useIDEContext.ts"
+import {saveProject} from "../../core/save"
+import { useHotkeys } from 'react-hotkeys-hook'
 
 export function useKeyboardShortcuts() {
   const {blocklyWorkspaceRef, setHasUnsavedFileChanges} = useIDEContext()
 
-  useEffect(() => {
-    const handler = (e: KeyboardEvent) => {
-      if ((e.metaKey || e.ctrlKey) && e.key === 's') {
-        e.preventDefault()
-        if (blocklyWorkspaceRef.current) {
-          saveProject({workspace: blocklyWorkspaceRef.current})
-          setHasUnsavedFileChanges(false)
-        }
-      }
+  useHotkeys('mod+s', () => {
+    if (blocklyWorkspaceRef.current) {
+      saveProject({workspace: blocklyWorkspaceRef.current})
+      setHasUnsavedFileChanges(false)
     }
+  })
 
-    window.addEventListener('keydown', handler)
-    return () => window.removeEventListener('keydown', handler)
-  }, [blocklyWorkspaceRef, setHasUnsavedFileChanges])
+  useHotkeys('mod+o', () => {
+    // TODO open project
+  })
+
+  useHotkeys('mod+n', () => {
+    // TODO new project
+  })
+
+  useHotkeys('mod+b', () => {
+    // TODO build project
+  })
+
+  useHotkeys('mod+i', () => {
+    // TODO inspect output
+  })
 }
